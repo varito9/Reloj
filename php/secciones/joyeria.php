d33461ea76b091e93606eedfb942032c54f4d62a
Varitoo9 <alvarosaldanaaa@gmail.com>
2025-07-01 17:15:01 +0200

    Se coloca el apartado de joyeria

--- php/secciones/joyeria.php
+++ php/secciones/joyeria.php
@@ -4,40 +4,85 @@ include '../utils/header.php';
 
 
 <main class="container my-5">
-  <h2 class="text-center mb-2">Servicios Varox</h2>
-  <p class="text-center text-muted mb-4">Soluciones rápidas, profesionales y personalizadas</p>
+  <h2 class="text-center mb-2">Colección de Joyería</h2>
+  <p class="text-center text-muted mb-4">Piezas elegantes, exclusivas y atemporales diseñadas para cada momento especial</p>
 
-  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 text-center">
+  <div class="row gy-4">
+        <!-- Imagen 0 -->
+    <div class="col-md-6 d-flex">
+      <div class="card flex-fill border-0 bg-oro-100 shadow-sm p-3">
+        <div class="row g-3 align-items-center">
+          <div class="col-3 text-center">
+          <img src="https://i.etsystatic.com/8626284/r/il/ee2e0a/1455368243/il_570xN.1455368243_f2os.jpg" alt="Anillo de compromiso" class="img-fluid rounded shadow-sm">
 
-    <div class="col">
-      <div class="p-4 bg-oro-100 rounded shadow-sm h-100">
-        <i class="bi bi-tools fs-1 text-dark mb-3"></i>
-        <h6 class="fw-semibold">Reparación Técnica</h6>
-        <p class="small text-muted">Reparamos y mantenemos tus relojes con piezas originales.</p>
+            <i class="bi bi-gem fs-1 text-dark"></i>
+          </div>
+
+          <!-- Texto 0-->
+          <div class="col-9">
+            <h5 class="card-title mb-1">Anillos de Compromiso</h5>
+            <p class="card-text small">Diseños únicos en oro blanco, amarillo o rosa con diamantes certificados. Zafiro Eternal <span class="fw-bold">™</span> Collection.</p>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="col">
-      <div class="p-4 bg-oro-100 rounded shadow-sm h-100">
-        <i class="bi bi-pencil-square fs-1 text-dark mb-3"></i>
-        <h6 class="fw-semibold">Grabado</h6>
-        <p class="small text-muted">Personaliza tus piezas con mensajes únicos y elegantes.</p>
+        <!-- Imagen 1 -->
+    <div class="col-md-6 d-flex">
+      <div class="card flex-fill border-0 bg-oro-100 shadow-sm p-3">
+        <div class="row g-3 align-items-center">
+          <div class="col-3 text-center">
+            <img src="https://img.joomcdn.net/a4b4b85536d118c867e288171bf7aba50defc019_original.jpeg" alt="Anillo de compromiso" class="img-fluid rounded shadow-sm">
+            <i class="bi bi-circle-half fs-1 text-dark"></i>
+          </div>
+
+        <!-- Texto 1-->
+          <div class="col-9">
+            <h5 class="card-title mb-1">Pendientes de Lujo</h5>
+            <p class="card-text small">Aros, perlas, y modelos colgantes con piedras preciosas. Diseño moderno y artesanal.</p>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="col">
-      <div class="p-4 bg-oro-100 rounded shadow-sm h-100">
-        <i class="bi bi-person-lines-fill fs-1 text-dark mb-3"></i>
-        <h6 class="fw-semibold">Asesoramiento</h6>
-        <p class="small text-muted">Te ayudamos a elegir la mejor opción según tus gustos.</p>
+        <!-- Imagen 2 -->
+    <div class="col-md-6 d-flex">
+      <div class="card flex-fill border-0 bg-oro-100 shadow-sm p-3">
+        <div class="row g-3 align-items-center">
+          <div class="col-3 text-center">
+
+          <img src="https://img.joomcdn.net/783c12e9b1b5f4b8e1821a1ceda76f034d7cb5dd_original.jpeg" alt="Anillo de compromiso" class="img-fluid rounded shadow-sm">
+
+            <i class="bi bi-link fs-1 text-dark"></i>
+          </div>
+
+         <!-- Texto 2 -->
+
+          <div class="col-9">
+            <h5 class="card-title mb-1">Pulseras Exclusivas</h5>
+            <p class="card-text small">Colección de pulseras con eslabones de diseño italiano. Combinación de elegancia y resistencia.</p>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="col">
-      <div class="p-4 bg-oro-100 rounded shadow-sm h-100">
-        <i class="bi bi-shield-check fs-1 text-dark mb-3"></i>
-        <h6 class="fw-semibold">Garantía Extendida</h6>
-        <p class="small text-muted">Protección total hasta 5 años con servicio premium.</p>
+
+        <!-- Imagen 3 -->
+    <div class="col-md-6 d-flex">
+      <div class="card flex-fill border-0 bg-oro-100 shadow-sm p-3">
+        <div class="row g-3 align-items-center">
+          <div class="col-3 text-center">
+            <i class="bi bi-bezier2 fs-1 text-dark"></i>
+            <img src="https://polkistories.com/cdn/shop/files/Necklace-1.jpg?v=1751007528" alt="Anillo de compromiso" class="img-fluid rounded shadow-sm">
+
+          </div>
+        <!-- Texto 3 -->
+
+          <div class="col-9">
+            <h5 class="card-title mb-1">Collares con Historia</h5>
+            <p class="card-text small">Inspirados en épocas clásicas. Cada collar cuenta una historia, cada gema una emoción.</p>
+          </div>
+        </div>
       </div>
     </div>
 
